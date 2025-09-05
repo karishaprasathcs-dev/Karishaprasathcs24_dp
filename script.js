c94e3acdb3eac9fb4c69f1521d6536852babb2ee
Karishaprasathcs <karishaprasathcs@gmail.com>
2025-09-05 13:58:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+program.html
+
+1
+
+<DOCTYPE html>
+
+2
+
+<htal>
+
+3
+
+<head>
+
+4
+
+5
+
+<title>My First Javascript Program</title>
+
+</head>
+
+6
+
+<body>
+
+7
+
+8
+
+9
+
+chi Welcome to Javascript</hi>
+
+<script>
+
+// your Javascript code will go here
+
+10
+
+</script>
+
+11
+
+e
+
+</body>
+
+12
+
+</html>
